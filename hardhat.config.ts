3a0aef92abaa99ad02d3b46fc97a6b65928c007b
Luka <luka.dimnik88@gmail.com>
2022-11-13 16:46:49 +0200

    Add goerli network to the hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv/config';
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   solidity: '0.8.17',
 };
 
