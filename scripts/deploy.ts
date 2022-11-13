4219a4606bb08f62f441e833e4511ae1c00fdf17
Luka <luka.dimnik88@gmail.com>
2022-11-13 16:36:51 +0200

    Deploying to local hardhat blockchain

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,22 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const lockedAmount = ethers.utils.parseEther('1');
+  // const Lock = await ethers.getContractFactory('Lock');
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // await lock.deployed();
+  // console.log(
+  //   `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // );
 
-  const lockedAmount = ethers.utils.parseEther('1');
-
-  const Lock = await ethers.getContractFactory('Lock');
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
+  console.log('Deploying contract...');
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed contract to: ${simpleStorage.address}`);
 }
 
 main()
