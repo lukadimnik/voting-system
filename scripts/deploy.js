dd20ca38984e164320a35a0ae2a89462090386eb
Luka <luka.dimnik88@gmail.com>
2022-11-13 14:02:41 +0200

    Deploy to goerli test network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,14 +4,17 @@ require('dotenv').config();
 
 async function main() {
   // connect to ganache instance
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.GOERLI_RPC_URL
+  );
   // connect to the wallet with the private key from an account on ganache
   const encryptedJson = fs.readFileSync('./.encryptedKey.json', 'utf8');
-  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-    encryptedJson,
-    process.env.PRIVATE_KEY_PASSWORD
-  );
-  wallet = await wallet.connect(provider);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //   encryptedJson,
+  //   process.env.PRIVATE_KEY_PASSWORD
+  // );
+  // wallet = await wallet.connect(provider);
   const abi = fs.readFileSync(
     './compiled-contracts/contracts_SimleStorage_sol_SimpleStorage.abi',
     'utf-8'
@@ -26,6 +29,7 @@ async function main() {
   const contract = await contractFactory.deploy();
   // wait one block to get the receipt
   await contract.deployTransaction.wait(1);
+  console.log(`Deployed to contract address: ${contract.address}`);
 
   const currentFavoriteNumber = await contract.retrieve();
   console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
