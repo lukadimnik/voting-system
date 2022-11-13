8ce1818970f9d7dcc6565b6e82083c80de20fc09
Luka <luka.dimnik88@gmail.com>
2022-11-13 15:05:17 +0200

    Initialize hardhat project

--- contracts/AccessNFT.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract AccessNft is ERC721 {
-    constructor() ERC721("Doggie", "DOG") {}
-}
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,47 +1,23 @@
-import { ethers } from 'ethers';
-import * as fs from 'fs-extra';
-import 'dotenv/config';
+import { ethers } from "hardhat";
 
 async function main() {
-  // connect to ganache instance
-  const provider = new ethers.providers.JsonRpcProvider(
-    process.env.GOERLI_RPC_URL
-  );
-  // connect to the wallet with the private key from an account on ganache
-  const encryptedJson = fs.readFileSync('./.encryptedKey.json', 'utf8');
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
-  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-  //   encryptedJson,
-  //   process.env.PRIVATE_KEY_PASSWORD
-  // );
-  // wallet = await wallet.connect(provider);
-  const abi = fs.readFileSync(
-    './compiled-contracts/contracts_SimleStorage_sol_SimpleStorage.abi',
-    'utf-8'
-  );
-  const binary = fs.readFileSync(
-    './compiled-contracts/contracts_SimleStorage_sol_SimpleStorage.bin',
-    'utf-8'
-  );
-  // wallet will deploy the contract
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log('Deploying, please wait...');
-  const contract = await contractFactory.deploy();
-  // wait one block to get the receipt
-  await contract.deployTransaction.wait(1);
-  console.log(`Deployed to contract address: ${contract.address}`);
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const currentFavoriteNumber = await contract.retrieve();
-  console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
-  const transactionResponse = await contract.store('8');
-  const transactionReceipt = await transactionResponse.wait(1);
-  const updatedFavoriteNumber = await contract.retrieve();
-  console.log(`Updated Favorite Number: ${updatedFavoriteNumber.toString()}`);
+  const lockedAmount = ethers.utils.parseEther("1");
+
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  await lock.deployed();
+
+  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
-  });
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
