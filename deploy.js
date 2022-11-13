01a0a3541209eca941f9acc780bc68e6cc19625b
Luka <luka.dimnik88@gmail.com>
2022-11-13 08:21:19 +0200

    Deploy contract to ganache private chain

--- deploy.js
+++ deploy.js
@@ -1,5 +1,26 @@
+const ethers = require('ethers');
+const fs = require('fs-extra');
+
 async function main() {
-  console.log('to be continued');
+  const provider = new ethers.providers.JsonRpcProvider(
+    'HTTP://127.0.0.1:7545'
+  );
+  const wallet = new ethers.Wallet(
+    '9a87bdc4b8f39a1c438e32f1c01431527a38f2cbaf2ef400e10ae4b88f3e96d3',
+    provider
+  );
+  const abi = fs.readFileSync(
+    './contracts_SimleStorage_sol_SimpleStorage.abi',
+    'utf-8'
+  );
+  const binary = fs.readFileSync(
+    './contracts_SimleStorage_sol_SimpleStorage.bin',
+    'utf-8'
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log('Deploying, please wait...');
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 
 main().then(() =>
