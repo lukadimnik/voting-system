389afb973e39046c4ab7743d5b061d15d6aaa7c2
Luka <luka.dimnik88@gmail.com>
2022-11-14 19:15:39 +0200

    Add gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,12 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import 'dotenv/config';
 import './tasks/block-number';
+import('hardhat-gas-reporter');
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY!;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || 'https://eth-goerli';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key';
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key';
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
@@ -23,6 +25,13 @@ const config: HardhatUserConfig = {
   solidity: '0.8.17',
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
 };
 
