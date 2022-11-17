14e239c99f2acd074592c802262ac3458afe2e30
Luka <luka.dimnik88@gmail.com>
2022-11-17 21:46:56 +0200

    Create a simple ERC20 token

--- contracts/NewToken.sol
+++ contracts/NewToken.sol
@@ -2,31 +2,10 @@
 
 pragma solidity 0.8.17;
 
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract NewToken {
-  uint256 favoriteNumber;
-
-  struct People {
-    uint256 favoriteNumber;
-    string name;
-  }
-  // uint256[] public anArray;
-  People[] public people;
-
-  mapping(string => uint256) public nameToFavoriteNumber;
-
-  function store(uint256 _favoriteNumber) public {
-    favoriteNumber = _favoriteNumber;
-  }
-
-  function retrieve() public view returns (uint256) {
-    return favoriteNumber;
-  }
-
-  function addPerson(string memory _name, uint256 _favoriteNumber) public {
-    people.push(People(_favoriteNumber, _name));
-    nameToFavoriteNumber[_name] = _favoriteNumber;
+contract NewToken is ERC20 {
+  constructor(uint256 initalSupply) ERC20('SimpleToken', 'ST') {
+    _mint(msg.sender, initalSupply);
   }
 }
