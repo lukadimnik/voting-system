d35229b180713233bf78735402eb1c7bb8f89221
Luka <luka.dimnik88@gmail.com>
2022-11-20 14:38:55 +0200

    Voting script for 4 addresses

--- scripts/voting.ts
+++ scripts/voting.ts
@@ -1,11 +1,34 @@
 import { ethers, getNamedAccounts } from 'hardhat';
 import { Ballot } from '../typechain-types/contracts/Ballot';
+import { stringArrtoByte32Arr } from '../utils/toByte32';
+import { bytesToString } from '../utils/parseBytes32';
 
 async function main() {
-  const { deployer } = await getNamedAccounts();
+  const { deployer, user1, user2, user3, user4 } = await getNamedAccounts();
   console.log('Voting script...');
   const ballot: Ballot = await ethers.getContract('Ballot', deployer);
   console.log('Wallet holds token: ', await ballot.walletHoldsToken());
+  await ballot.giveRightToVote(user1);
+  await ballot.giveRightToVote(user2);
+  await ballot.giveRightToVote(user3);
+  await ballot.giveRightToVote(user4);
+  console.log('Deployer voting...');
+
+  await ballot.vote(1);
+  console.log('User1 voting...');
+
+  const ballot1: Ballot = await ethers.getContract('Ballot', user1);
+  await ballot1.vote(0);
+  const ballot2: Ballot = await ethers.getContract('Ballot', user2);
+  await ballot2.vote(0);
+  const ballot3: Ballot = await ethers.getContract('Ballot', user3);
+  await ballot3.vote(0);
+  const ballot4: Ballot = await ethers.getContract('Ballot', user4);
+  await ballot4.vote(0);
+
+  const winnerName = await ballot.winnerName();
+
+  console.log('🚀winnerName: ', await bytesToString(winnerName));
 }
 
 main()
