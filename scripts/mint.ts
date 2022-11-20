86afc82e46a50c9b47d889140f635578ce0059b0
Luka <luka.dimnik88@gmail.com>
2022-11-20 17:03:35 +0200

    Refactor minting script

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -3,11 +3,9 @@ import { BasicNft } from '../typechain-types/contracts/BasicNFT.sol/BasicNft';
 
 async function main() {
   const { deployer, user1, user2, user3, user4 } = await getNamedAccounts();
-  const basicNft: BasicNft = await ethers.getContract('BasicNft', deployer);
-
   console.log('Minting...');
-  const transactionRes = await basicNft.mintNft();
-
+  const basicNft: BasicNft = await ethers.getContract('BasicNft', deployer);
+  await basicNft.mintNft();
   const basicNft1: BasicNft = await ethers.getContract('BasicNft', user1);
   await basicNft1.mintNft();
   const basicNft2: BasicNft = await ethers.getContract('BasicNft', user2);
@@ -16,6 +14,7 @@ async function main() {
   await basicNft3.mintNft();
   const basicNft4: BasicNft = await ethers.getContract('BasicNft', user4);
   await basicNft4.mintNft();
+  console.log('Minting finished.');
 
   // console.log('------');
   // await transactionRes.wait(1);
