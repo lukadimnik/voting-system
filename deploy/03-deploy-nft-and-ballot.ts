03bd61f57c03aa284639cf20f6687e7ecbb3eb7b
Luka <luka.dimnik88@gmail.com>
2022-11-20 08:47:42 +0200

    Create a NFT & Ballot deployment script for deploy command

---
+++ deploy/03-deploy-nft-and-ballot.ts
@@ -0,0 +1,54 @@
+import {
+  developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+} from '../helper-hardhat-config';
+import verify from '../utils/verify';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { BasicNft } from '../typechain-types/contracts/BasicNFT.sol/BasicNft';
+import { Ballot } from '../typechain-types/contracts/Ballot';
+
+const deployBasicNft: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
+  // @ts-ignore
+  const { deployments, getNamedAccounts, network, ethers } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS;
+
+  log('----------------------------------------------------');
+  log('Deploying BasicNft contract...');
+  const basicNftArgs: any[] = [];
+  const basicNft: BasicNft = await deploy('BasicNft', {
+    from: deployer,
+    args: basicNftArgs,
+    log: true,
+    waitConfirmations: waitBlockConfirmations || 1,
+  });
+  log('----------------------------------------------------');
+  log('Deploying Ballot contract...');
+  const ballotArgs: any[] = [basicNft.address];
+  const ballot: Ballot = await deploy('Ballot', {
+    from: deployer,
+    args: ballotArgs,
+    log: true,
+    waitConfirmations: waitBlockConfirmations || 1,
+  });
+
+  // Verify the deployment if not run on local network
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log('Verifying BasicNft contract...');
+    await verify(basicNft.address, basicNftArgs);
+    log('Verifying Ballot contract...');
+    await verify(ballot.address, ballotArgs);
+  }
+};
+
+export default deployBasicNft;
+deployBasicNft.tags = ['all', 'nftandballot', 'main'];
