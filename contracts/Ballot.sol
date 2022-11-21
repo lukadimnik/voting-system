bd88d00a7ecc5a4acab2b3656359a840cca425b4
Luka <luka.dimnik88@gmail.com>
2022-11-21 18:24:03 +0200

    Clean up the voting contract

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -2,69 +2,45 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import '@openzeppelin/contracts/interfaces/IERC721.sol';
-import 'hardhat/console.sol';
+// import 'hardhat/console.sol';
 
-/// @title Voting with delegation.
+/// @title Voting system.
 contract Ballot {
-  // This declares a new complex type which will
-  // be used for variables later.
-  // It will represent a single voter.
   struct Voter {
-    uint256 weight; // weight is accumulated by delegation
     bool voted; // if true, that person already voted
     uint256 vote; // index of the voted proposal
   }
 
-  // This is a type for a single proposal.
   struct Proposal {
     bytes32 name; // short name (up to 32 bytes)
     uint256 voteCount; // number of accumulated votes
   }
 
-  address public chairperson;
   address nftContractAddress;
 
-  // This declares a state variable that
-  // stores a `Voter` struct for each possible address.
   mapping(address => Voter) public voters;
 
-  // A dynamically-sized array of `Proposal` structs.
   Proposal[] public proposals;
 
-  /// Create a new ballot to choose one of `proposalNames`.
   constructor(bytes32[] memory proposalNames, address _nftContractAddress) {
     nftContractAddress = _nftContractAddress;
-    chairperson = msg.sender;
-    voters[chairperson].weight = 1;
 
-    // For each of the provided proposal names,
-    // create a new proposal object and add it
-    // to the end of the array.
     for (uint256 i = 0; i < proposalNames.length; i++) {
-      // 'Proposal({...})' creates a temporary
-      // Proposal object and 'proposals.push(...)'
-      // appends it to the end of 'proposals'.
       proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
     }
   }
 
-  /// Give your vote (including votes delegated to you)
-  /// to proposal `proposals[proposal].name`.
   function vote(uint256 proposal) external {
     require(
       IERC721(nftContractAddress).balanceOf(msg.sender) > 0,
       'Does not hold the right access NFT'
     );
     require(!voters[msg.sender].voted, 'The voter already voted.');
-    voters[msg.sender].weight = 1;
     Voter storage sender = voters[msg.sender];
     sender.voted = true;
     sender.vote = proposal;
 
-    // If `proposal` is out of the range of the array,
-    // this will throw automatically and revert all
-    // changes.
-    proposals[proposal].voteCount += sender.weight;
+    proposals[proposal].voteCount += 1;
   }
 
   /// @dev Computes the winning proposal taking all
@@ -79,18 +55,12 @@ contract Ballot {
     }
   }
 
-  // Calls winningProposal() function to get the index
-  // of the winner contained in the proposals array and then
-  // returns the name of the winner
   function winnerName() external view returns (bytes32 winnerName_) {
     winnerName_ = proposals[winningProposal()].name;
   }
 
   // will check if the caller has the right to vote
-  function walletHoldsToken() public view returns (bool) {
-    console.log('msg.sender: ', msg.sender);
-    console.log('nftContractAddress: ', nftContractAddress);
-
+  function isElligibleToVote() public view returns (bool) {
     return IERC721(nftContractAddress).balanceOf(msg.sender) > 0;
   }
 }
